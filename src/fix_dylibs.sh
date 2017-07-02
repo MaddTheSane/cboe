d98ed0f8380522883fdd07b4bec861311af099ae
C.W. Betts <computers57@hotmail.com>
2017-07-02 14:17:07 -0600

    More pokes and prodding.

--- src/fix_dylibs.sh
+++ src/fix_dylibs.sh
@@ -8,17 +8,18 @@
 
 echo Fixing boost dylib install names...
 
-EXEPATH=@executable_path/../Frameworks
+EXEPATH=@rpath/Frameworks
 BOEPATH="$BUILT_PRODUCTS_DIR/$EXECUTABLE_PATH"
+BSYPATH="$BUILT_PRODUCTS_DIR/$FRAMEWORKS_FOLDER_PATH/libboost_system.dylib"
 BFSPATH="$BUILT_PRODUCTS_DIR/$FRAMEWORKS_FOLDER_PATH/libboost_filesystem.dylib"
 BTHPATH="$BUILT_PRODUCTS_DIR/$FRAMEWORKS_FOLDER_PATH/libboost_thread.dylib"
 
 # Update references in the executable file
-install_name_tool -change libboost_system.dylib "$EXEPATH/libboost_system.dylib" "$BOEPATH"
-install_name_tool -change libboost_filesystem.dylib "$EXEPATH/libboost_filesystem.dylib" "$BOEPATH"
-install_name_tool -change libboost_thread.dylib "$EXEPATH/libboost_thread.dylib" "$BOEPATH"
+install_name_tool -change /usr/local/lib/libboost_system.dylib "$EXEPATH/libboost_system.dylib" \
+-change /usr/local/lib/libboost_filesystem.dylib "$EXEPATH/libboost_filesystem.dylib" \
+-change /usr/local/lib/libboost_thread.dylib "$EXEPATH/libboost_thread.dylib" "$BOEPATH"
 
 # Update references within Boost
-install_name_tool -change libboost_system.dylib "$EXEPATH/libboost_system.dylib" "$BFSPATH"
-install_name_tool -change libboost_system.dylib "$EXEPATH/libboost_system.dylib" "$BTHPATH"
-
+install_name_tool -change /usr/local/lib/libboost_system.dylib "$EXEPATH/libboost_system.dylib" -id "$EXEPATH/libboost_filesystem.dylib" "$BFSPATH"
+install_name_tool -change /usr/local/lib/libboost_system.dylib "$EXEPATH/libboost_system.dylib" -id "$EXEPATH/libboost_thread.dylib" "$BTHPATH"
+install_name_tool -id "$EXEPATH/libboost_system.dylib" "$BSYPATH"
